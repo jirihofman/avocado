672de1d24976155b65c5f33df2ec996629d08ebf
Jiří Hofman <jiri.hofman@gmail.com>
2023-06-02 22:07:11 +0200

    update

--- components/header.js
+++ components/header.js
@@ -6,13 +6,12 @@ export default function Header({ children }) {
         <header>
             <nav className='navbar navbar-expand-lg navbar-light bg-light'>
                 <div className='container-fluid'>
-                    <a className='navbar-brand' href='#'>🥑Avokádo (lekce lehce)</a>
+                    <a className='navbar-brand' href='/'>🥑 Home</a>
                     <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
                         <span className='navbar-toggler-icon' />
                     </button>
                     <div className='collapse navbar-collapse' id='navbarSupportedContent'>
                         <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-                            <li className='nav-item'><Link legacyBehavior href='/'><a className='nav-link' title='Homepage'>Home</a></Link></li>
                             <li className='nav-item'><Link legacyBehavior href='/posts'><a className='nav-link' title='Blog a novinky'>Blog</a></Link></li>
                             <li className='nav-item'><Link legacyBehavior href='/settings'><a className='nav-link disabled' title='Nastavení'>Nastavení</a></Link></li>
                             <li className='nav-item'><Link legacyBehavior href='https://github.com/jirihofman/avocado'><a className='nav-link' title='GitHub' target='_blank'>GitHub</a></Link></li>
