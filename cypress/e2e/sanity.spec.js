ed9c01cfca79e62b720722597144ee5adf2b45cd
Jiří Hofman <jiri.hofman@gmail.com>
2023-06-04 23:12:00 +0200

    update tests

--- cypress/e2e/sanity.spec.js
+++ cypress/e2e/sanity.spec.js
@@ -9,11 +9,6 @@ context('Sanity checks', () => {
             return false;
         });
     });
-    it('Header links', () => {
-        ['Blog', 'Nastavení', 'GitHub', 'O Aplikaci'].forEach(header => {
-            cy.get('header ul li a').contains(header).should('be.visible');
-        });
-    });
     it('Sections exist', () => {
         ['Demo', 'Plná verze'].forEach(header => {
             cy.get('main section h3').contains(header).should('be.visible');
